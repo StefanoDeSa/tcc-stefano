5791ffcdd0d8ce9f7d1f25cd3f356f7fc735dbcd
StefanoDeSa <stefanobritodesa@gmail.com>
2025-04-08 17:54:42 -0300

    feat: adiciona endpoint para buscar dados do usuário autenticado

--- tccstefano/api/viewsets.py
+++ tccstefano/api/viewsets.py
@@ -1,8 +1,19 @@
-from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope
-from rest_framework import generics, permissions
+from oauth2_provider.contrib.rest_framework import OAuth2Authentication
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .serializers import CustomUser, CustomUserSerializer
 
 class CustomUserList(generics.ListAPIView):
     permission_classes = []
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+    
+
+class MeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = CustomUserSerializer(request.user)
+        return Response(serializer.data)
--- tccstefano/urls.py
+++ tccstefano/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from oauth2_provider import urls as oauth2_urls
-from tccstefano.api.viewsets import CustomUserList
+from tccstefano.api.viewsets import CustomUserList, MeView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('o/', include(oauth2_urls)),
     path('api/users/', CustomUserList.as_view(), name='api_users'),
+    path('api/me/', MeView.as_view(), name='api_me'),
     path('', include('sso_auth.urls')),
 ]
