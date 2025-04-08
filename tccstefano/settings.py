85224133cf706a581b8b6d7f669b48a695fdaab0
StefanoDeSa <stefanobritodesa@gmail.com>
2025-04-08 17:55:18 -0300

    feat: adiciona django-cors-headers para permitir o fetch do endpoint

--- tccstefano/settings.py
+++ tccstefano/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'sso_auth',
     'oauth2_provider',
     'rest_framework',
+    'corsheaders',
 ]
 
 OAUTH2_PROVIDER = {
@@ -71,11 +72,16 @@ LOGIN_URL = '/login/'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = 'tccstefano.urls'
